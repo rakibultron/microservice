1e48f1523139a92d08e9f33811774b0d03723cb9
rakibul-dev <mdrakibul.dev@gmail.com>
2024-10-31 05:07:44 +0600

    feat(auth): add basic 'Hello World' route for auth service and configure Ingress NGINX
    - Added a basic 'Hello World' route in the auth service for initial testing and setup.
    - Configured Ingress NGINX to route traffic to the auth service.
    - Set up Ingress rules to handle HTTP traffic and route to the /api/auth endpoint.
    - Tested the Ingress configuration to ensure proper routing to the auth service.

--- tickr/auth/src/index.ts
+++ tickr/auth/src/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 app.use(express.json());
 
 const port: Number = 4000;
+
+app.get("/api/auth/users", async (req: Request, res: Response) => {
+  res.json({ data: ["Rakib", "Roni", "Raju"] });
+});
+
+app.get("/api/auth", async (req, res) => {
+  console.log(req.query.id);
+  res.json({ hello: "Auth service is running on port `${port}`" });
+});
 
 app.listen(port, () => {
   console.log("Auth service is running on port ==>", port);
