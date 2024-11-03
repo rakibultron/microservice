bf0253d869549b5ffb8c0236edf96c42a5283e8c
rakibul-dev <mdrakibul.dev@gmail.com>
2024-11-04 03:23:30 +0600

    feat: add login page

--- tickr/client/next.config.ts
+++ tickr/client/next.config.ts
@@ -1,4 +1,15 @@
 module.exports = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/",
+    "/auth",
+  ],
   images: {
     remotePatterns: [
       {
--- tickr/client/src/app/auth/login/page.tsx
+++ tickr/client/src/app/auth/login/page.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-const Page = () => {
-  return <div>Login page</div>;
-};
-
-export default Page;
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center flex-row  h-screen">
+      <div className="w-80">
+        <Card className="mx-auto max-w-sm">
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl font-bold">Login</CardTitle>
+            <CardDescription>
+              Enter your email and password to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
